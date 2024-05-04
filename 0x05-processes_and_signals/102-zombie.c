5101722afe2b94b3d36c1e66756d7a6e92affff3
Mwamtindi <mwamtindishaban@gmail.com>
2024-05-04 14:28:29 +0300

    Processes and signals

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * infinite_while - the infinite loop
+ *
+ * Return: 0
+ */
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - fxn to creates five zombie processes
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	pid_t p_id;
+	char count = 0;
+
+	while (count < 5)
+	{
+		p_id = fork();
+		if (p_id > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			count++;
+		}
+		else
+			exit (0);
+	}
+
+	infinite_while();
+	return (0);
+}
