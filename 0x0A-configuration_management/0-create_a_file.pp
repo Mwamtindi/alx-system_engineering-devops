5c1863ceb29a897355df8860fa9f3cbc4e024bca
Mwamtindi <mwamtindishaban@gmail.com>
2024-06-28 23:29:17 +0300

    create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# A Puppet manifest that creates a file at /tmp
+file { '/tmp/school':
+    ensure  => 'file',
+    path    => '/tmp/school',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
