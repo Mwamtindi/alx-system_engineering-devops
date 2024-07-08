94eeee1fe42c3e7a07a105b0436b625172b41ddb
Mwamtindi <mwamtindishaban@gmail.com>
2024-07-09 00:58:01 +0300

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Puppet that install flask from pip3
+# Puppet manifest to install Flask 2.1.0 and compatible Werkzeug
+package { 'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['werkzeug'],
 }
